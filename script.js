163a1412a383132ef615638c4f7d9fce4a18a988
nonoqwerty28 <nono.qwerty28@gmail.com>
2022-05-07 01:41:23 +0300

    second commit

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const middleContent = document.querySelector(".middle-content");
 const btnTop = document.querySelector(".btn-top");
 const newsFeedPage = document.querySelector(".feeds-page");
 const loginModal = document.querySelector(".login-modal");
+const modalX = document.querySelector(".login-modal i");
+const loginFormBtn = document.querySelector(".login-form-btn");
+const postBtn = document.querySelector(".post-btn");
+const modalWrapper = document.querySelector(".modal-wrapper");
+const modal = document.querySelector(".modal");
+const postModalX = document.querySelector(".modal-header i");
+const modalPostBtn = document.querySelector(".modal-header button");
+const modalFooterPlus = document.querySelector(".modal-footer span");
+const modalInput = document.querySelector(".modal-input");
 /********************************************************/
 /********************************************************/
 
@@ -30,5 +39,56 @@ btnTop.addEventListener("click", () => {
   } else {
     goToLoginPage();
     loginModal.style.display = "block";
+  }
+});
+
+// Login page
+modalX.addEventListener("click", () => {
+  loginModal.style.display = "none";
+});
+
+loginFormBtn.addEventListener("click", () => {
+  const loginUserInfo = document.querySelector(".login-user-info");
+  const loginPassword = document.querySelector(".login-password");
+
+  if (loginUserInfo.value !== "" && loginPassword.value !== "") {
+    loginPage.style.display = "none";
+    newsFeedPage.style.display = "block";
+  } else {
+    loginModal.style.display = "block";
+  }
+});
+
+// News feed page
+// Post modal
+postBtn.addEventListener("click", () => {
+  modal.style.display = "block";
+  modalWrapper.classList.add("modal-wrapper-display");
+});
+
+const changeOpacity = (x) => {
+  modalPostBtn.style.opacity = x;
+  modalFooterPlus.style.opacity = x;
+};
+
+postModalX.addEventListener("click", () => {
+  modal.style.display = "none";
+  modalWrapper.classList.remove("modal-wrapper-display");
+
+  if (modalInput.value !== "") {
+    modalInput.value = "";
+    changeOpacity(0.5);
+  }
+});
+
+modalInput.addEventListener("keypress", (e) => {
+  if (e.target.value !== "") {
+    changeOpacity(1);
+  }
+});
+
+modalInput.addEventListener("blur", (e) => {
+  if (e.target.value === "") {
+    changeOpacity(0.5);
   }
 });
